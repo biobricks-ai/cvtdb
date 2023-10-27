d30d56b0b3f926d17329a67e0be5c7142fafabf4
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-10-27 11:51:29 -0400

    Add pipeline to get SQLite file

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -6,30 +6,15 @@
 localpath=$(pwd)
 echo "Local path: $localpath"
 
-# Create the list directory to save list of remote files and directories
-listpath="$localpath/list"
-echo "List path: $listpath"
-mkdir -p $listpath
-cd $listpath;
-
-# Define the FTP base address
-ftpbase=""
-
-# Retrieve the list of files to download from FTP base address
-wget --no-remove-listing $ftpbase
-cat index.html | grep -Po '(?<=href=")[^"]*' | sort | cut -d "/" -f 10 > files.txt
-rm .listing
-rm index.html
-
 # Create the download directory
 downloadpath="$localpath/download"
 echo "Download path: $downloadpath"
 mkdir -p "$downloadpath"
-cd $downloadpath;
 
-# Download files in parallel
-cat $listpath/files.txt | xargs -P14 -n1 bash -c '
-echo $0
-wget -nH -q -nc -P '$downloadpath' '$ftpbase'$0'
+# Define the tarball URL
+tarball_url="https://github.com/USEPA/CompTox-PK-CvTdb/archive/refs/heads/master.tar.gz"
+
+# Retrieve and unpack the files
+curl -L $tarball_url | tar -C $downloadpath -xvzf -
 
 echo "Download done."
---
+++ stages/02_build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Select SQLite files from download
+
+# Get local path
+localpath=$(pwd)
+echo "Local path: $localpath"
+
+# Set download path
+downloadpath="$localpath/download"
+echo "Download path: $downloadpath"
+
+# Create brick directory
+brickpath="$localpath/brick"
+mkdir -p $brickpath
+echo "Brick path: $brickpath"
+
+# Copy SQLite file to brick
+find $downloadpath -maxdepth 2 -type f -name '*.sqlite' \
+  -exec cp -pv {} $brickpath \;
